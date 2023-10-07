3c21823b9660f02673471398f799ab86fbe0d7e3
Nithinreddy44 <122263554+Nithinreddy44@users.noreply.github.com>
2023-10-07 12:44:24 +0530

    Create matplotlib.py

---
+++ matplotlib.py
@@ -0,0 +1,11 @@
+#import the below external modules to python 
+import matplotlib.pyplot as plt
+import numpy as np
+#use import keyword to import modules
+
+xpoints=np.array([1,3])
+ypoints=np.array([3,1,120])
+xpoints=np.array([1,2,6,8])
+ypoints=np.array([3,8,1,10])
+plt.plot(xpoints,ypoints)
+plt.show()
